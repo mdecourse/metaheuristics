ac166f275cf559559a5004bdb0a1a79179856b7b
KmolYuan <pyslvs@gmail.com>
2019-01-14 18:02:12 +0800

    Update setup script.

--- setup.py
+++ setup.py
@@ -2,38 +2,50 @@
 
 """Compile the Cython libraries of Pyslvs."""
 
+from os import listdir
 from distutils.core import setup, Extension
-import os
-
+from platform import system
 from Cython.Distutils import build_ext
 import numpy
 
+np_include = numpy.get_include()
+
 
 sources = []
-for source in os.listdir("./src"):
-    if source.split('.')[-1] == 'pyx':
-        sources.append(source)
+for place in ["src/"]:
+    for source in listdir(place):
+        if source.split('.')[-1] == 'pyx':
+            sources.append(place + source)
 
-extra_compile_args = [
-    # Compiler optimize.
-    '-O3',
-    # Disable NumPy warning only on Linux.
-    '-Wno-cpp',
-    # Windows format warning.
-    '-Wno-format',
+macros = [
+    ('_hypot', 'hypot'),
 ]
 
+compile_args = [
+    '-O3',
+]
+
+if system() == 'Windows':
+    # Avoid compile error with CYTHON_USE_PYLONG_INTERNALS.
+    # https://github.com/cython/cython/issues/2670#issuecomment-432212671
+    macros.append(('MS_WIN64', None))
+    # Disable format warning.
+    compile_args.append('-Wno-format')
+    # Disable NumPy warning.
+    macros.append(('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION'))
 
 # Original src
 ext_modules = []
 for source in sources:
     ext_modules.append(Extension(
-        source.split('.')[0],
-        sources=['src/' + source],  # path + file name
-        
-        include_dirs=[numpy.get_include()],
-        extra_compile_args=extra_compile_args,
+        # Base name
+        source.split('/')[-1].split('.')[0],
+        # path + file name
+        sources=[source],
+        language="c",
+        include_dirs=[np_include],
+        define_macros=macros,
+        extra_compile_args=compile_args,
     ))
-
 
 setup(ext_modules=ext_modules, cmdclass={'build_ext': build_ext})
