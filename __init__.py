27983c84156b61dd2e7aa980683c98f10dcbf31f
KmolYuan <pyslvs@gmail.com>
2019-10-19 22:30:02 +0800

    Expose "AlgorithmBase" in root.

--- __init__.py
+++ __init__.py
@@ -2,13 +2,14 @@
 
 """Kernel of Metaheuristic Random Algorithm."""
 
-from .verify import Verification
+from .verify import Verification, AlgorithmBase
 from .rga import Genetic
 from .firefly import Firefly
 from .de import Differential
 
 __all__ = [
     'Verification',
+    'AlgorithmBase',
     'Genetic',
     'Firefly',
     'Differential',
